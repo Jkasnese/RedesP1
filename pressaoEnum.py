c422865bac6e15818125aec6a791934a4d3d0780
Guilherme <guilherme@localhost.localdomain>
2017-09-25 13:53:44 -0300

    Criando classe sensor e enum pressao

---
+++ pressaoEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Pressao(Enum):
+    NORMAL = 0
+    BAIXA = 1
+    ALTA = 2
---
+++ sensor.py
@@ -0,0 +1,15 @@
+import pressaoEnum
+
+class Sensor:
+    """Classe responsavel pela definicao dos sensores.
+       Atributos:
+        int bmp
+        bool movimento
+        Pressao pressao"""
+
+def __init__(self, bmp, movimento, pressao):
+    self.bmp = bmp
+    self.movimento = movimento
+    self.pressao = pressao
+
+
